728cd0cd7bb3dc0b9766a5eaa6b8ebafac8078a5
yanghaisheng <1304684124@qq.com>
2018-11-18 17:56:57 +0800

    修改GitTest

--- git-test-project/src/GitTest.java
+++ git-test-project/src/GitTest.java
@@ -2,10 +2,12 @@ public class GitTest {
     public static void main(String[] args)  {
         System.out.println("烦哦，烦人的东西");
         System.out.println("烦哦，烦人的东西");
+        // hello test
     }
 
     public void test(){
         int i =1;
         i += 11;
+        System.out.println(i);
     }
 }
